f4a7dd454bb600b8fb6efc1325bfcfd6b78f3e20
Budya <budya2012@gmail.com>
2018-11-01 15:10:52 +0300

    authorization stage

--- PersonalPortal/PersonalPortal/App/index.jsx
+++ PersonalPortal/PersonalPortal/App/index.jsx
@@ -1,9 +1,22 @@
 ﻿import React from 'react' //1
 import { render } from 'react-dom' //2
+import { createStore, applyMiddleware} from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 import App from './containers/app.jsx' //3
+import blogReduser from './containers/blog/blogReducer.jsx'
+
+function configureStore(initialState) {
+    return createStore(blogReduser,
+                       initialState,
+                       applyMiddleware(thunk));
+}
+
+const store = configureStore();
 
 render(
-    <App />,
-    document.getElementById('content')
-) //4
-
+    <Provider store={store}>
+        <App />    
+    </Provider>,
+document.getElementById('content')
+)
